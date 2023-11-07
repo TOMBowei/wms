23a93120c772e2efe249270875b2323098d2c03a
BoweiWu <91042301+TOMBowei@users.noreply.github.com>
2023-11-07 16:54:03 +0800

    hello wms

---
+++ sbwms/src/main/java/com/wms/sbwms/SbwmsApplication.java
@@ -0,0 +1,13 @@
+package com.wms.sbwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbwmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbwmsApplication.class, args);
+    }
+
+}
---
+++ sbwms/src/test/java/com/wms/sbwms/SbwmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wms.sbwms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbwmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
