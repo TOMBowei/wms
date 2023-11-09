2aeb519356c32c846474ca5513579fa9215eb340
BoweiWu <91042301+TOMBowei@users.noreply.github.com>
2023-11-09 10:23:22 +0800

    add delete update query

--- sbwms/src/main/java/com/wms/sbwms/controller/UserController.java
+++ sbwms/src/main/java/com/wms/sbwms/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.wms.sbwms.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.wms.sbwms.entity.User;
 import com.wms.sbwms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +26,32 @@ public class UserController {
     public List<User> list(){
         return userService.list();
     }
-
+//    新增
+    @PostMapping("/save")
+    public boolean save(@RequestBody User user){
+        return userService.save(user);
+    }
+//    修改
+    @PostMapping("/update")
+    public boolean update(@RequestBody User user){
+        return userService.updateById(user);
+    }
+//    新增或修改
+    @PostMapping("/saveOrUpdate")
+    public boolean saveOrUpdate(@RequestBody User user){
+        return userService.saveOrUpdate(user);
+    }
+//    删除
+    @GetMapping("/delete")
+    public boolean delete(Integer id){
+        return userService.removeById(id);
+    }
+//    查询(模糊、匹配)
+    @GetMapping("/query")
+    public List<User> query(@RequestBody User user){
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+//        wrapper.like(User::getName, user.getName());
+        wrapper.eq(User::getName, user.getName());
+        return userService.list(wrapper);
+    }
 }
